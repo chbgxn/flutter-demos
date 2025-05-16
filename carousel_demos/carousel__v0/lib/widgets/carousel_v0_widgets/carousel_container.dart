835ce62e3661e2d7ce486b7a658aa5147e6f427c
DevGalaxyCh <devCosmicH@gmail.com>
2025-05-16 13:48:00 +0800

    Add files via upload

---
+++ carousel_demos/carousel__v0/lib/widgets/carousel_v0_widgets/carousel_container.dart
@@ -0,0 +1,29 @@
+// Author: @DevGalaxyCh
+import 'package:flutter/material.dart';
+import 'slider_me.dart';
+
+class CarouselContainer extends StatelessWidget {
+  final String img;
+  final int currentIndex;
+  
+
+  const CarouselContainer({required this.img,required this.currentIndex,super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      margin: EdgeInsets.only(top: 10),
+      child: Stack(
+        children: [
+          Image.asset(img),
+          Positioned(
+            bottom: 10,
+            left: 100,
+            child: SliderMe(currentIndex: currentIndex),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ carousel_demos/carousel__v0/lib/widgets/carousel_v0_widgets/slider_me.dart
@@ -0,0 +1,33 @@
+// Author: @DevGalaxyCh
+import 'package:flutter/material.dart';
+
+class SliderMe extends StatelessWidget {
+  final int currentIndex;
+  const SliderMe({required this.currentIndex, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 20,
+      width: 150,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8)
+      ),
+      child: ListView.builder(
+        itemCount: 5,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            height: 20, 
+            width: 20, 
+            margin: EdgeInsets.only(left: 5),
+            decoration: BoxDecoration(
+              color: currentIndex==index?Colors.orange:Colors.white,
+              shape: BoxShape.circle
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
