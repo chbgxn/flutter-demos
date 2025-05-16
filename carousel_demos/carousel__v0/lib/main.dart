432773f92f8309579796bccfaeed7051604b7a77
DevGalaxyCh <devCosmicH@gmail.com>
2025-05-16 13:47:02 +0800

    Update main.dart

--- carousel_demos/carousel__v0/lib/main.dart
+++ carousel_demos/carousel__v0/lib/main.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/material.dart';
+import 'widgets/carousel_v0.dart';
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("轮播图_V0_Author: @DevGalaxyCh")),
+        body: CarouselV0(),
+      ),
+    );
+  }
+}
