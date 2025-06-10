646cf278d8640a89a0fbe7a9f96bd8f839f17b70
chbgxn <devCosmicH@gmail.com>
2025-06-10 17:12:29 +0800

    add file daily

---
+++ snippets/animation/hero_naive_demo.dart
@@ -0,0 +1,54 @@
+/*
+- [EN] Learn how to use Hero animation
+- Introductions:
+- An naive Hero animation for personal learning.
+
+- [ZH] 学习如何使用Hero动画
+- 介绍：
+- 一个简单的Hero动画，用于个人学习。
+
+- Author: @chbgxn
+*/
+
+import 'package:flutter/material.dart';
+
+class HeroNaiveDemo extends StatelessWidget {
+  const HeroNaiveDemo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('页面1')),
+      body: Center(
+        child: GestureDetector(
+          onTap:() => Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context)=>const NewPage())
+          ),
+          child: Hero(
+            tag: 'img_example', 
+            child: Image.asset('assets/images/example.jpg', height: 100),
+          ),
+        )
+        
+      ),
+    );
+  }
+}
+
+class NewPage extends StatelessWidget {
+  const NewPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('页面2')),
+      body: Center(
+        child: Hero(
+          tag: 'img_example',  
+          child: Image.asset('assets/images/example.jpg', height: 300),
+        ),
+      ),
+    );
+  }
+}
