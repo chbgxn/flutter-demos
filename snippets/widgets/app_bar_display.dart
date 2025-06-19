c64215a18f46340a8e056c7489059a8feea3da63
chbgxn <devCosmicH@gmail.com>
2025-06-19 20:25:51 +0800

    Update

--- snippets/widgets/app_bar_display.dart
+++ snippets/widgets/app_bar_display.dart
@@ -5,10 +5,10 @@
 When greater than or equal to the set value, AppBar will display.
 - Referring to the handling of AppBar on personal homepage by Taobao and Pinduoduo, and implement it.
 
-- [ZH] 控制AppBar的显示与消失
+- [ZH] 控制 AppBar 的显示与消失
 - 介绍：
-- 设置了一个AppBar，当滚动总偏移量小于设定值时，AppBar不会显示；大于等于设定值时，AppBar会显示。
-- 参考了淘宝、拼多多在个人主页对AppBar的处理，并实现。
+- 设置了一个 AppBar，当滚动总偏移量小于设定值时，AppBar 不会显示；大于等于设定值时，AppBar 会显示。
+- 参考了淘宝、拼多多在个人主页对 AppBar 的处理，并实现。
 
 - Author: chbgxn
 */
